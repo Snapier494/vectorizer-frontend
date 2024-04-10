e64dc175c6b84d45e588a02c37ed79bc1e9127b8
Snapier494 <snapier494@gmail.com>
2024-04-10 03:39:29 +0300

    update code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   const [imageFile, setImageFile] = useState(null);
   const [loading, setLoading] = useState(false); // State to track loading
   const [vectorizedData, setVectorizedData] = useState(null); // State to hold vectorized data
+  const [maxColors, setMaxColors] = useState(0); // New state for max colors
+  const [antiAliasing, setAntiAliasing] = useState('anti_aliased'); // Default value set to 'anti_aliased'
+  const [drawStyle, setDrawStyle] = useState('fill_shapes'); // Default value set to 'stroke_shapes'
+  const [useOverrideColor, setUseOverrideColor] = useState('false'); // Default value set to 'stroke_shapes'
 
   const handleImageChange = (event) => {
     console.log('file upload');
@@ -15,11 +19,35 @@ function App() {
     }
   };
 
+  const handleMaxColorsChange = (event) => {
+    const value = parseInt(event.target.value);
+    if (value >= 0 && value <= 256) {
+      setMaxColors(value);
+    }
+  };
+
+  const handleDrawStyleChange = (event) => {
+    setDrawStyle(event.target.value);
+  };
+
+  const handleAntiAliasingChange = (event) => {
+    setAntiAliasing(event.target.value);
+  };
+
+  const handleUserOverriedColorChange = (event) => {
+    setUseOverrideColor(event.target.value);
+  }
+
   const handleSubmit = () => {
     if (imageFile) {
+      setVectorizedData(null);
       setLoading(true); // Set loading to true while request is being processed
       const formData = new FormData();
       formData.append('imageData', imageFile);
+      formData.append('processingMax_colors', maxColors);
+      formData.append('outputBitmapAnti_aliasing_mode', antiAliasing)
+      formData.append('outputDraw_style', drawStyle);
+      formData.append('outputStokesUse_override_color', useOverrideColor);
 
       fetch('http://localhost:8000/vectorize', {
         method: 'POST',
@@ -66,7 +94,63 @@ function App() {
         <button onClick={handleSubmit} className="btn" disabled={loading}>
           {loading ? 'Loading...' : 'Vectorize'}
         </button>
-      </div>      
+      </div>
+      {/* Display max colors value */}
+      <div style={{ marginTop: '20px' }}>
+        <label htmlFor="maxColors">
+          Max Colors:&nbsp;
+        </label>
+        <input
+          type="number"
+          name="maxColors"
+          id="maxColors"
+          className="number-input"
+          min="0"
+          max="256"
+          value={maxColors}
+          onChange={handleMaxColorsChange}
+        />
+      </div>
+      {/* Add comboBox for antiAliasing */}
+      <div style={{ marginTop: '20px' }}>
+        <label htmlFor="antiAliasing">Anti-Aliasing:&nbsp;</label>
+        <select
+          id="antiAliasing"
+          name="antiAliasing"
+          value={antiAliasing}
+          onChange={handleAntiAliasingChange}
+        >
+          <option value="anti_aliased">Anti-Aliased</option>
+          <option value="aliased">Aliased</option>
+        </select>
+      </div>
+      {/* Add comboBox for drawStyle */}
+      <div style={{ marginTop: '20px' }}>
+        <label htmlFor="drawStyle">Draw-Style:&nbsp;</label>
+        <select
+          id="drawStyle"
+          name="drawStyle"
+          value={drawStyle}
+          onChange={handleDrawStyleChange}
+        >
+          <option value="fill_shapes">fill_shapes</option>
+          <option value="stroke_shapes">stroke_shapes</option>
+          <option value="stroke_edges">stroke_edges</option>
+        </select>
+      </div>
+      {/* Add comboBox for overriedColor */}
+      <div style={{ marginTop: '20px' }}>
+        <label htmlFor="drawStyle">Override-Color:&nbsp;</label>
+        <select
+          id="overrideColor"
+          name="overrideColor"
+          value={useOverrideColor}
+          onChange={handleUserOverriedColorChange}
+        >
+          <option value="false">false</option>
+          <option value="true">true</option>
+        </select>
+      </div>
       {/* Display the uploaded image */}
       {imageFile &&
         <>
